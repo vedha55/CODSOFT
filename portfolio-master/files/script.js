a0d50508b4d2870b693d69e67d0a950fbafd67ef
vedha55 <110324502+vedha55@users.noreply.github.com>
2024-06-01 14:13:59 +0530

    Add files via upload

---
+++ portfolio-master/files/script.js
@@ -0,0 +1,19 @@
+
+ typed = new Typed(".text", {
+    strings: ["Programming" , "Frontend Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
